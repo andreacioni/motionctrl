9d338f0ebacc247fb2496a574e022f25a341ee91
Andrea <supercionci@hotmail.it>
2018-02-11 18:59:37 +0100

    Stream proxy work!

--- api/stream.go
+++ api/stream.go
@@ -5,32 +5,38 @@ import (
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
+	"strconv"
 
 	"../motion"
 )
 
 func bridgeStream(w io.Writer) bool {
 	resp, err := http.Get(motion.GetStreamBaseURL())
+	boundary := motion.MotionStreamBoundary
 
 	if err != nil {
 		return false
 	}
 
-	mr := multipart.NewReader(resp.Body, motion.MotionStreamBoundary)
+	mr := multipart.NewReader(resp.Body, boundary)
 	p, err := mr.NextPart()
 	if err == io.EOF {
 		return false
-	}
-	if err != nil {
+	} else if err != nil {
 		return false
 	}
+
 	stream, err := ioutil.ReadAll(p)
 	if err != nil {
 		return false
 	}
 
-	_, err = w.Write(stream)
+	bodyLength := strconv.Itoa(len(stream))
 
+	stream = append([]byte("--"+boundary+"\r\nContent-type: image/jpeg\r\nContent-Length:"+bodyLength+"\r\n\r\n"), stream...)
+	stream = append(stream, []byte("\r\n")...)
+
+	_, err = w.Write(stream)
 	if err != nil {
 		return false
 	}
