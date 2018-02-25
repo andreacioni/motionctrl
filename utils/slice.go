797ffb560c592127ba11055d82b3afedc25f3cc3
Andrea <supercionci@hotmail.it>
2018-02-25 12:27:42 +0100

    BlockSlideSlice can now stop

--- utils/slice.go
+++ utils/slice.go
@@ -22,17 +22,18 @@ func InSlice(val interface{}, array interface{}) (exists bool, index int) {
 	return
 }
 
-func BlockSlideSlice(array interface{}, blockSize int, f func(interface{})) {
+func BlockSlideSlice(array interface{}, blockSize int, f func(interface{}) bool) {
+	var run bool
 	switch reflect.TypeOf(array).Kind() {
 	case reflect.Slice:
 		s := reflect.ValueOf(array)
 		n := s.Len()
 
-		for i := 0; i < n; i += blockSize {
+		for i := 0; (i < n) && run; i += blockSize {
 			if i+blockSize > n { //TODO improve here (?)
-				f(s.Slice(i, n).Interface())
+				run = f(s.Slice(i, n).Interface())
 			} else {
-				f(s.Slice(i, i+blockSize).Interface())
+				run = f(s.Slice(i, i+blockSize).Interface())
 			}
 		}
 	}
