d1e06569a82694e62bbd1e60b69d8108e7e75c5d
Andrea <supercionci@hotmail.it>
2018-02-26 15:48:37 +0100

    fixing utils test

--- utils/size_test.go
+++ utils/size_test.go
@@ -7,22 +7,22 @@ func TestFromTextSize(t *testing.T) {
 	val, err := FromTextSize("121B")
 
 	require.NoError(t, err)
-	require.Equal(t, uint64(121), val)
+	require.Equal(t, int64(121), val)
 
 	val, err = FromTextSize("122KB")
 
 	require.NoError(t, err)
-	require.Equal(t, uint64(122000), val)
+	require.Equal(t, int64(122000), val)
 
 	val, err = FromTextSize("123MB")
 
 	require.NoError(t, err)
-	require.Equal(t, uint64(123000000), val)
+	require.Equal(t, int64(123000000), val)
 
 	val, err = FromTextSize("124GB")
 
 	require.NoError(t, err)
-	require.Equal(t, uint64(124000000000), val)
+	require.Equal(t, int64(124000000000), val)
 
 	_, err = FromTextSize("125")
 
