fea7c08818464bc0a97e228e23288f6131b17d73
Andrea <supercionci@hotmail.it>
2018-03-07 22:17:22 +0100

    new net utility function IsLocalIP and tests

---
+++ utils/net.go
@@ -0,0 +1,35 @@
+package utils
+
+import "net"
+
+func IsLocalIP(localIP net.IP) (bool, error) {
+	ifaces, err := net.Interfaces()
+
+	if err != nil {
+		return false, err
+	}
+
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+
+		if err != nil {
+			return false, err
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			if ip.Equal(localIP) {
+				return true, nil
+			}
+		}
+	}
+
+	return false, nil
+}
---
+++ utils/net_test.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsLocalIPTrue(t *testing.T) {
+	ip := net.ParseIP("127.0.0.1")
+
+	require.NotNil(t, ip)
+
+	res, err := IsLocalIP(ip)
+
+	require.NoError(t, err)
+	require.True(t, res)
+
+	ip = net.ParseIP("118.123.123.231")
+
+	require.NotNil(t, ip)
+
+	res, err = IsLocalIP(ip)
+
+	require.NoError(t, err)
+	require.False(t, res)
+}
