210e4fb8e23ae143b414d046cc236a9752a63ac7
andreacioni <supercionci@hotmail.it>
2018-04-25 00:13:54 +0200

    new command backup method

---
+++ backup/cmd.go
@@ -0,0 +1,12 @@
+package backup
+
+type CmdBackupService struct {
+}
+
+func (b *CmdBackupService) Upload(file string) error {
+	return nil
+}
+
+func (b *CmdBackupService) Authenticate() error {
+	return nil
+}
