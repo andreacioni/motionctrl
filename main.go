e9b7de84843e5737c7c230daba09a558b2dc883d
Andrea Cioni <supercionci@hotmail.it>
2020-09-13 17:41:54 +0200

    exit motionctrl on error in init phase

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/kpango/glg"
@@ -38,30 +39,36 @@ func main() {
 	//Load motionctrl configuration file
 	if err := config.Load(configFile); err != nil {
 		glg.Fatalf("Error loading configuration: %v", err)
+		os.Exit(1)
 	}
 
 	//Initialize motion package
 	if err := motion.Init(config.GetConfig().MotionConfigFile, autostart, detection); err != nil {
 		glg.Fatalf("Error initializing motion package: %v", err)
+		os.Exit(1)
 	}
 
 	//Initialize backup  (if enabled)
 	if targetDir, err := motion.ConfigGet(motion.ConfigTargetDir); err == nil && targetDir != nil {
 		if err := backup.Init(config.GetBackupConfig(), targetDir.(string)); err != nil {
 			glg.Errorf("Error initializing backup package: %v", err)
+			os.Exit(1)
 		}
 	} else {
 		glg.Errorf("Unable to build backup service without valid 'target_dir' configured")
+		os.Exit(1)
 	}
 
 	//Initialize notify  (if enabled)
 	if err := notify.Init(config.GetNotifyConfig()); err != nil {
 		glg.Errorf("Error initializing notify package: %v", err)
+		os.Exit(1)
 	}
 
 	//Initialize REST api
 	if err := api.Init(config.GetConfig(), shutdownHook); err != nil {
 		glg.Errorf("Error initializing API package: %v", err)
+		os.Exit(1)
 	}
 }
 
